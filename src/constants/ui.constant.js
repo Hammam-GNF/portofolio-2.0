f033a92d4ae81f27edb9df4c0a1ba56499a8bcc7
Hammam Ghina Nur Fauzi <hammamgonjil@gmail.com>
2026-07-22 14:23:58 +0700

    refactor: extract shared footer layout component

--- src/constants/ui.constant.js
+++ src/constants/ui.constant.js
@@ -2,3 +2,10 @@ export const TOGGLE_BUTTON = {
     SHOW_MORE: "See More",
     SHOW_LESS: "See Less",
 };
+
+export const FOOTER_CONFIG = {
+    YEAR: 2026,
+    BRAND: "HammamGNF™",
+    URL: "https://www.hammam.my.id",
+    COPYRIGHT: "All Rights Reserved",
+};
--- src/layouts/LandingLayout.jsx
+++ src/layouts/LandingLayout.jsx
@@ -1,26 +1,15 @@
+import Footer from "../components/layout/Footer";
+
+
 const LandingLayout = ({ children }) => {
     return (
         <>
             {children}
 
-            <footer>
-                <center>
-                    <hr className="my-3 border-gray-400 opacity-15 sm:mx-auto lg:my-6 text-center" />
-
-                    <span className="block text-sm pb-4 text-gray-500 dark:text-gray-400">
-                        © 2026{" "}
-                        <a
-                            href="www.hammam.my.id"
-                            className="hover:underline"
-                        >
-                            HammamGNF™
-                        </a>
-                        . All Rights Reserved.
-                    </span>
-                </center>
-            </footer>
+            <Footer />
         </>
     );
 };
+
 
 export default LandingLayout;
